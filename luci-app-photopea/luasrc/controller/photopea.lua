31956913e19bc8ea9fd2f52bf61424f9674b6b37
3wlh <1094890624@qq.com>
2026-03-17 18:01:46 +0800

    更新 photopea.lua

--- luci-app-photopea/luasrc/controller/photopea.lua
+++ luci-app-photopea/luasrc/controller/photopea.lua
@@ -1,28 +1,23 @@
-module("luci.controller.photopea", package.seeall)
+local name = "photopea"
+module("luci.controller."..name, package.seeall)
 
 function index()
-	entry({"admin", "services", "photopea"}, firstchild(), _("Photopea"), 90).dependent = true
-	entry({"admin", "services","photopea_status"}, call("Run_status"))
+	entry({"admin", "services", name}, firstchild(), _("Photopea"), 90).dependent = true
+	entry({"admin", "services",name.."_status"}, call("Run_status"))
 	-- 注册菜单 
-	entry({"admin", "services", "photopea", "settings"}, cbi("photopea/settings"), _("Settings"), 10).leaf = true
-	entry({"admin", "services", "photopea", "edit"}, template("photopea/edit"), _("Edit"), 20).leaf = true
-	entry({"admin", "services", "photopea", "file"}, template("photopea/files"), _("Files"), 30).leaf = true
-	entry({"admin", "services", "photopea", "logs"}, template("photopea/logs"), _("Logs"), 40).leaf = true
+	entry({"admin", "services", name, "settings"}, cbi(name.."/settings"), _("Settings"), 10).leaf = true
+	entry({"admin", "services", name, "edit"}, template(name.."/edit"), _("Edit"), 20).leaf = true
+	entry({"admin", "services", name, "file"}, template(name.."/files"), _("Files"), 30).leaf = true
+	entry({"admin", "services", name, "logs"}, template(name.."/logs"), _("Logs"), 40).leaf = true
 end
 
 function Run_status()
 	local uci  = require "luci.model.uci".cursor()
-	local port = tonumber(uci:get("photopea", "config", "port"))
-	local token = uci:get("photopea", "config", "token")
-	local file_bin = "photopea"
-	local find_cmd = "find /usr/sbin/ -maxdepth 1 -name photopea* -exec basename {} \\; | head -1"
-    local fp = io.popen(find_cmd, "r")
-    if fp then
-        file_bin = fp:read("*a"):gsub("^%s+", ""):gsub("%s+$", "")
-        fp:close()
-    end
+	local port = tonumber(uci:get(name, "config", "port"))
+	local token = uci:get(name, "config", "token")
+	local cmd = string.format("pgrep %s* >/dev/null", name)
 	local status = {
-		running = (luci.sys.call("pidof "..file_bin.." >/dev/null") == 0),
+		running = (luci.sys.call(cmd),
 		port = (port or 8887),
 		token = (token or "")
 	}
