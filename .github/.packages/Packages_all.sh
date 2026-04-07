98636c942ab910ad024f822d40f1aecbaa2529f6
3wlh <3wlh@3wlh.cn>
2026-04-07 23:00:03 +0800

    更新 Packages_all.sh

--- .github/.packages/Packages_all.sh
+++ .github/.packages/Packages_all.sh
@@ -9,9 +9,9 @@ Openclash "${ARCH}" "${PACKAGES_PATH}"
 # Socat "${ARCH}" "${PACKAGES_PATH}"
 Releases "${ARCH}" "${PACKAGES_PATH}" "ports-status luci-app-napcatapi luci-app-scriptrun luci-app-mddns"
 Segmentation "${PACKAGES_PATH}" "https://dl.openwrt.ai/releases/24.10/packages/${ARCH}/kiddin9/" \
-"luci-app-unishare unishare webdav2 luci-app-v2ray-server"
+"luci-app-unishare unishare webdav2 luci-app-v2ray-server sunpanel"
 Segmentation "${PACKAGES_PATH}" "https://istore.istoreos.com/repo/all/nas_luci/" \
-"luci-app-socat luci-i18n-socat-zh-cn luci-app-sunpanel luci-i18n-sunpanel-zh-cn sunpanel"
+"luci-app-socat luci-i18n-socat-zh-cn luci-app-sunpanel luci-i18n-sunpanel-zh-cn"
 Segmentation "${PACKAGES_PATH}" "https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/packages/${ARCH}/luci/" \
 "luci-app-homeproxy luci-i18n-homeproxy-zh-cn luci-app-ramfree luci-i18n-ramfree-zh-cn 
 luci-app-argon-config luci-i18n-argon-config-zh-cn luci-theme-argon"
@@ -19,4 +19,3 @@ Segmentation "${PACKAGES_PATH}" "https://istore.istoreos.com/repo/all/store/" \
 "taskd luci-lib-xterm luci-lib-taskd luci-app-store"
 Segmentation "${PACKAGES_PATH}" "https://downloads.openwrt.org/releases/24.10.4/packages/${ARCH}/packages/" \
 "docker_"
-
