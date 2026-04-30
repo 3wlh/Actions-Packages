3b5d9e5267873523adc0bffc2be7aaeb3870214d
3wlh <3wlh@3wlh.cn>
2026-04-30 22:56:44 +0800

    up

--- .github/script/Download.sh
+++ .github/script/Download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 [[ $(curl -o /dev/null -s --head -w "%{http_code}" "${1}") -ge 400 ]] && exit
 echo "Downloading ${1}"
-find ${2} -maxdepth 1 -type f -name "$(echo "$(basename ${1})" | cut -d "_" -f1)*" -exec rm -f {} \;
+find ${2} -type f -name "$(echo "$(basename ${1})" | cut -d "_" -f1)*" -exec rm -f {} \;
 curl -# -L --fail "${1}" -o "$(pwd)/$(basename ${1})"
 # wget -qO "$(pwd)/$(basename ${1})" "${1}" --show-progress
 if [[ "$(du -b $(pwd)/$(basename ${1}) 2>/dev/null | awk '{print $1}')" -le "512" ]]; then
--- .github/script/Segmentation.sh
+++ .github/script/Segmentation.sh
@@ -15,7 +15,7 @@ while IFS= read -r LINE; do
             fi
             Download_URL="${PACKAGES_URL}${FILE}"
             if [[ ! -f "${PACKAGES_PATH}/${FILE}" ]];then
-                find "${PACKAGES_PATH}" -maxdepth 1 -type f -name "${NAME}*" -exec rm -f {} \;
+                find "${PACKAGES_PATH}" -type f -name "${NAME}*" -exec rm -f {} \;
                 Download "${PACKAGES_PATH}" "${Download_URL}"
             else
                 # 5. 【修复】修复颜色代码 \e 无法被正确解析的问题
